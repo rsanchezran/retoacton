a947c6ab7e9f6a801148164c84846f81b51a49f8
lapzarin <brenni1310>
2020-02-12 11:31:50 -0600

    Prueba de webhook

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -40,16 +40,17 @@ class ApiController extends Controller
     public function webhook(Request $request)
     {
         Log::info($request->all());
-        if ($request->data["object"] != null) {
-            if (array_key_exists('order_id', $request->data["object"])) {
-                $order_id = $request->data["object"]["order_id"];
-                $cobro = $request->data["object"]["amount"];
+        $object = $request->object;
+        if ($object != null) {
+            if (array_key_exists('order_id', $object)) {
+                $order_id = $object["order_id"];
+                $cobro = $object["amount"];
                 $contacto = Contacto::where("order_id", $order_id)->first();
                 if ($contacto !== null) {
                     $usuario = User::where('email', $contacto->email)->first();
                     if ($usuario == null) {
                         User::crear($contacto->nombres, $contacto->apellidos, $contacto->email,
-                            $request->data["object"]["payment_method"]["type"], 0, $contacto->codigo, $cobro);
+                            $object["payment_method"]["type"], 0, $contacto->codigo, $cobro);
                     } else {
                         $usuario->refrendarPago($cobro, $contacto->telefono);
                     }
