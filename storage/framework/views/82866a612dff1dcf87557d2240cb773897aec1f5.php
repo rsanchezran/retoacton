921ede6a3f02027bbe270010298b68d2c4dce03e
lapzarin <david.lazarin@tresfactorial.com>
2020-05-18 12:40:24 -0500

    Se corrige asignacion de inicio de reto

--- storage/framework/views/82866a612dff1dcf87557d2240cb773897aec1f5.php
+++
@@ -1,5 +0,0 @@
-
-            <?php echo e($contacto->nombre." ".$contacto->apellidos); ?> tiene la siguiente duda
-            <?php echo e($contacto->mensaje); ?>
-
-<?php /**PATH /var/www/resources/views/correo/duda_plano.blade.php ENDPATH**/ ?>
