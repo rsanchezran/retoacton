59fd293099f92ee759dae327cf1a23949946c64c
lapzarin <david.lazarin@tresfactorial.com>
2020-07-01 17:25:16 -0500

    Se agrega comando para registrar clientes por parte de algun desarrollador

--- app/Console/Commands/RegistrarCliente.php
+++ app/Console/Commands/RegistrarCliente.php
@@ -2,13 +2,15 @@
 
 namespace App\Console\Commands;
 
+use App\Code\MedioContacto;
+use App\Contacto;
 use App\Job;
 use App\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
-class ProcesarVideo extends Command
+class RegistrarCliente extends Command
 {
     /**
      * The name and signature of the console command.
@@ -43,6 +45,17 @@ class ProcesarVideo extends Command
     {
         $user = $this->argument('cliente');
         $usuario = json_decode($user);
+        $contacto = new Contacto();
+        $contacto->nombres = $usuario->nombres;
+        $contacto->apellidos = $usuario->apellidos;
+        $contacto->email = $usuario->email;
+        $contacto->telefono = $usuario->telefono;
+        $contacto->codigo = $usuario->codigo;
+        $contacto->objetivo = 0;
+        $contacto->etapa = 5;
+        $contacto->medio = MedioContacto::OTRO;
+        $contacto->save();
+
         User::crear($usuario->nombres, $usuario->apellidos, $usuario->email, $usuario->tipo_pago, 0,
             $usuario->codigo, $usuario->cobro);
     }
