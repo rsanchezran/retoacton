e2b6c46fb33b2c0faa0ef2593a8c7c927d450e9c
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2020-12-08 20:03:39 -0600

    Adecuaciones Retos

--- resources/views/reto/dia.blade.php
+++ resources/views/reto/dia.blade.php
@@ -174,7 +174,7 @@
                         <h4 class="comida">Calendario</h4>
                         <div class="d-flex m-auto col-12 col-sm-6">
                             <select class="selectpicker" v-model="semana" @change="mostrarSemana(semana)">
-                                <option v-for="s in p_semana" :value="s">Semana @{{ s }}</option>
+                                <option v-for="s in p_semana+1" :value="s">Semana @{{ s }}</option>
                             </select>
                         </div>
                         <div class="d-flex flex-wrap ">
