e137fe6a31216e79fa3273bd6cbd8d649ac39d0f
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-01-08 21:18:09 -0600

    Buscador vacio

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -197,7 +197,7 @@ class UserController extends Controller
             $usuarios = $usuarios->where('colonia', $campos->colonia);
         }
         if ($campos->codigo_personal != null) {
-            var_dump($campos->codigo_personal);
+            var_dump($usuarios);
             $usuarios = $usuarios->where('referencia', strtoupper($campos->codigo_personal));
         }
         /*if ($campos->estado != 0) {
