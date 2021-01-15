81ae6dc4a403fd6ee82d9d2d45fab09c74845bf2
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-01-15 09:44:11 -0600

    Correcciones landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -686,15 +686,6 @@
                 <div id="inicioFeature">
                     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-pause="false">
                         <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img class="d-block w-100" src="{{asset('img/landing3.jpg')}}" alt="Third slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="{{asset('img/landing2.jpg')}}" alt="Second slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="{{asset('img/landing1.jpg')}}" alt="Forth slide">
-                            </div>
                             <div class="carousel-item">
                                 <img class="d-block w-100" src="{{asset('images/imagesremodela/INDIVIDUALES_BANNER_LIFE.jpg')}}" alt="First slide">
                             </div>
