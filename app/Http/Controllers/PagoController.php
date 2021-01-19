3c85494edbcc708b50d692d2194619bfa98b7801
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-01-19 00:19:01 -0600

    Correcciones landing

--- app/Http/Controllers/PagoController.php
+++ app/Http/Controllers/PagoController.php
@@ -201,7 +201,12 @@ class PagoController extends Controller
             $usuario == null ? null : $usuario->inicio_reto,
             $usuario == null ? null : $usuario->deleted_at)->monto;
 
-        if($usuario->dias_paso !== 0){
+        $d = explode('00', $usuario->dias_paso);
+        if($d[1]){
+            if($d[0] == 14){$cobro=500;}
+            if($d[0] == 28){$cobro=1000;}
+            if($d[0] == 56){$cobro=2000;}
+            if($d[0] == 84){$cobro=3000;}
             if($usuario->saldo>$cobro) {
                 $cobro = ($cobro - $usuario->saldo) * 100;
             }else{
