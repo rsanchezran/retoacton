06f73aa335e55f1676ebce599c0908e1156ce36b
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-01-21 18:13:23 -0600

    Correcciones landing

--- app/Http/Controllers/PagoController.php
+++ app/Http/Controllers/PagoController.php
@@ -348,7 +348,7 @@ class PagoController extends Controller
             /*if(intval($d[0]) == 28){$cobro=1000;}
             if(intval($d[0]) == 56){$cobro=2000;}
             if(intval($d[0]) == 84){$cobro=3000;}*/
-            if($d[1]) {
+            if(isset($d[1])) {
                 if (intval($d[1]) == 1) {
                     if ($usuario->saldo < $cobro) {
                         $cobro = ($cobro - $usuario->saldo);
