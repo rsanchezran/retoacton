116179d6ddeefa94605cb91c0c2c05369931e546
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-02-02 10:09:04 -0600

    Correcciones landing

--- app/Http/Controllers/RetoController.php
+++ app/Http/Controllers/RetoController.php
@@ -237,8 +237,8 @@ class RetoController extends Controller
             }
         }
         $diasTranscurridosuno = Carbon::now()->startOfDay()->diffInDays($inicioReto);
-        if($diasTranscurridos >= $user->dias){
-            $diasTranscurridos = $user->dias-1;
+        if($diasTranscurridos >= intval($user->dias)){
+            $diasTranscurridos = intval($user->dias);
         }
         $teorico = $diasTranscurridosuno;
         if ($teorico == 0) {
