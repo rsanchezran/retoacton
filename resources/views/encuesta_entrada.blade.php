1a09eb5acbc6637724683ad2dde05f1db58fe983
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-02-04 10:31:26 -0600

    Correcciones landing

--- resources/views/encuesta_entrada.blade.php
+++ resources/views/encuesta_entrada.blade.php
@@ -183,10 +183,10 @@
                     </div>
                 </div>
                 <div v-else class="card-header text-center" style="padding: 20px; font-size: 1.2rem;">
-                    <div v-if="!this.empieza">
+                    <div v-if="!this.empieza && this.num_pregunta == 8">
                         A continuación te mostraré como es que se desarrolla tu programa
                     </div>
-                    <div v-else>
+                    <div v-if="this.empieza || this.num_pregunta != 8">
                         @{{ pregunta }}
                     </div>
                 </div>
