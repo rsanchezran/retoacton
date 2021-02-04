cedb89985632745c2934880e943b37ac0251a6aa
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-02-04 10:50:28 -0600

    Correcciones landing

--- resources/views/encuesta_entrada.blade.php
+++ resources/views/encuesta_entrada.blade.php
@@ -183,7 +183,7 @@
                     </div>
                 </div>
                 <div v-else class="card-header text-center" style="padding: 20px; font-size: 1.2rem;">
-                    <div v-if="!this.videointermedio || !this.quitavideo">
+                    <div v-if="!this.videointermedio && this.num_pregunta == 8">
                         A continuación te mostraré como es que se desarrolla tu programa
                     </div>
                     <div v-if="this.videointermedio || this.num_pregunta != 8">
