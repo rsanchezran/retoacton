91b015c991a0ade66e93712ae3913e3aa102af61
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-02-05 09:44:14 -0600

    Correcciones landing

--- resources/views/encuesta_entrada.blade.php
+++ resources/views/encuesta_entrada.blade.php
@@ -348,6 +348,10 @@
                 comprobarAbiertas: function () {//comprueba errores con las preguntas (cerradas y abiertas)
                     let vm = this;
                     vm.errors = [];
+                    document.cookie = "nombre=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                    document.cookie = "apellidos=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                    document.cookie = "telefono=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                    document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
                     if(document.getElementById("14").value != '' && document.getElementById("15").value != '' && document.getElementById("16").value != '' && document.getElementById("17").value != '' ){
                         this.nombre_ = document.getElementById("14").value;
                         this.apellidos_ = document.getElementById("15").value;
@@ -551,6 +555,7 @@
             created: function () {
                 let vm = this;
                 vm.user = vm.p_user;
+                document.cookie = "ksdoi=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
                 vm.p_preguntas.forEach(function (item) { //separar Preguntas Abiertas de Cerradas
                     if (item.multiple == undefined)
                         vm.preguntasAbiertas.push(item);
