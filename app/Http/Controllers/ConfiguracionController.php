14db1c021616b303614ebc1ecd278fe7b833a591
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-03-01 12:30:50 -0600

    Correcciones landing

--- app/Http/Controllers/ConfiguracionController.php
+++ app/Http/Controllers/ConfiguracionController.php
@@ -76,9 +76,11 @@ class ConfiguracionController extends Controller
     public function detalle_video(Request $request, $video){
         $this->authorize('configurar.videos');
         $videos = VideosPublicos::all();
-        foreach ($videos as $video) {
-            $videos->push(['nombre' => $video->nombre, 'src' => url('/getVideo/') . "/$video->nombre/" . rand(1, 100)]
-            );
+        foreach ($videos as $v) {
+            if($v->nombre == $video) {
+                $videos->push(['nombre' => $v->nombre, 'src' => url('/getVideo/') . "/$v->nombre/" . rand(1, 100)]
+                );
+            }
         }
         $categorias = Categoria::all();
         foreach ($categorias as $categoria) {
