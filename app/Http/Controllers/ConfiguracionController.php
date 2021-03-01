61998ff017d7cf61561d7f83a9e6685fe9e42345
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-03-01 12:59:59 -0600

    Correcciones landing

--- app/Http/Controllers/ConfiguracionController.php
+++ app/Http/Controllers/ConfiguracionController.php
@@ -90,7 +90,9 @@ class ConfiguracionController extends Controller
         }
         $pendientes = $this->getVideosPendientes();
 
-        return view('configuracion.videos_publicos', ['videos' => url('/getVideo/') . "/$video/" . rand(1, 100),
+        $url_video = str_replace ( ' ', '%20', $video);
+
+        return view('configuracion.videos_publicos', ['videos' => url('/getVideo/') . "/$url_video/" . rand(1, 100),
             'categorias' => $categorias, 'pendientes' => $pendientes, 'nombre' => ucfirst($video) ]);
     }
 
