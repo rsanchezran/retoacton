85ba51e9ab6738a19daaf60e81e1c84248d1c840
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-10-05 14:33:13 -0500

    Correccines encuesta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -709,13 +709,13 @@
                         </div>
                         <a class="carousel-control-prev" href="#carouselExampleControls" role="button"
                            data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
+                            <span class="carousel-control-prev-icon" style="display: none !important;" aria-hidden="true"></span>
+                            <span class="sr-only" style="display: none !important;">Previous</span>
                         </a>
                         <a class="carousel-control-next" href="#carouselExampleControls" role="button"
                            data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
+                            <span class="carousel-control-next-icon" style="display: none !important;" aria-hidden="true"></span>
+                            <span class="sr-only" style="display: none !important;">Next</span>
                         </a>
                     </div>
                 </div>
@@ -809,12 +809,12 @@
                         <a class="carousel-control-prev" href="#carouselExampleControls" role="button"
                            data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
+                            <span class="sr-only" style="display: none !important;">Previous</span>
                         </a>
                         <a class="carousel-control-next" href="#carouselExampleControls" role="button"
                            data-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
+                            <span class="sr-only" style="display: none !important;">Next</span>
                         </a>
                     </div>
                 </div>
