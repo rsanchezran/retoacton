a6845b3516fa28d11960383b840fde6d44c801fb
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-10-08 13:21:26 -0500

    correcciones encuesta

--- app/Http/Controllers/RetoController.php
+++ app/Http/Controllers/RetoController.php
@@ -341,7 +341,7 @@ class RetoController extends Controller
                     }
                 }
                 $alimentosIgnorados = Dieta::whereIn('comida', $ignorar)->get()->pluck('id');
-                $objetivo = Pregunta::where('pregunta', 'like', '%Objetivo fitness%')->first();
+                $objetivo = Pregunta::where('pregunta', 'like', '%Mi objetivo%')->first();
                 $preguntaPeso = Pregunta::where('pregunta', 'like', '%peso%')->first();
                 $objetivo = strpos($respuestas->get($objetivo->id)->respuesta, "Bajar") ? 'bajar' : 'subir';
                 $peso = json_decode($respuestas->get($preguntaPeso->id)->respuesta);
