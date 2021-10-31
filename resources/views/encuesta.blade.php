10d422dda59d9eff2d815c5ced56052de532f5d4
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-10-31 07:48:27 -0600

    Suplementos

--- resources/views/encuesta.blade.php
+++ resources/views/encuesta.blade.php
@@ -390,9 +390,9 @@
                                         <label v-else for="staticEmail" class="col-6 col-form-label" :class="sexotext2">@{{ pregunta.pregunta }}</label>
                                         <div class="col-6">
                                             <input v-if="pregunta.pregunta  == 'Estatura'" class="form-control encuesta" :class="sexo" v-model="pregunta.respuesta"
-                                               :placeholder="pregunta.pregunta" >
+                                               :placeholder="pregunta.pregunta" :keydown="keyhander">
                                             <input v-else class="form-control encuesta" :class="sexo" v-model="pregunta.respuesta"
-                                               :placeholder="Kg'">
+                                               :placeholder="Kg">
                                             <form-error align="left" :name="pregunta.pregunta+'.respuesta'"
                                                 :errors="errors" class="col-12 text-center"></form-error>
                                         </div>
@@ -1036,6 +1036,13 @@
                     this.archivo2 = this.$refs.archivo2.files[0];
                     this.subir_archivo_2();
                 },
+                keyhandler: function(event) {
+                    if (event.key == ".")
+                    {
+                        // do something
+                        event.preventDefault();
+                    }
+                    },
             },
             created: function () {
                 let vm = this;
