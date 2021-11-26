10af1867ddaa509ce8e9d00412378e4a82225600
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2021-11-26 16:09:09 -0600

    coins

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof \Illuminate\Session\TokenMismatchException) {
+
+            return redirect('/');
+
+        }
         return parent::render($request, $exception);
     }
     
