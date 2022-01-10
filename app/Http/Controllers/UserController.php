e883d0175008f02469f467abe8415820076253b1
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2022-01-10 14:57:14 -0600

    coins

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1451,7 +1451,7 @@ class UserController extends Controller
         $ids = array($filt[0]['codigo'],$filt[1]['codigo'],$filt[2]['codigo'],$filt[3]['codigo'],$filt[4]['codigo'],$filt[5]['codigo'],$filt[6]['codigo'],$filt[7]['codigo'],$filt[8]['codigo'],$filt[9]['codigo']);
         $ids_ordered = implode(',', $ids);
 
-        $usuarios = User::whereIn('referencia', $ids)->orderByRaw("FIELD(refencia, $ids_ordered)");
+        $usuarios = User::whereIn('referencia', $ids)->orderByRaw("FIELD(referencia, $ids_ordered)");
 
         $usuarios = $usuarios->select(['users.*'])->paginate(15);
 
