f2b1358cb2958254bd232755d86b416cf336132e
ricardo.sanchez17r <ricardo.sanchez17r@gmail.com>
2022-01-31 21:20:22 -0600

    coins

--- app/Http/Controllers/RetoController.php
+++ app/Http/Controllers/RetoController.php
@@ -339,7 +339,7 @@ class RetoController extends Controller
             $alimentosIgnorados = Dieta::whereIn('comida', $ignorar)->get()->pluck('id');
             $objetivo = Pregunta::where('pregunta', 'like', '%Mi objetivo%')->first();
             $preguntaPeso = Pregunta::where('pregunta', 'like', '%peso%')->first();
-            $objetivo = strpos($respuestas->get($objetivo->id)->respuesta, "Bajar") ? 'bajar' : 'subir';
+            $objetivo = strpos($respuestas->get(13)->respuesta, "Bajar") ? 'bajar' : 'subir';
             $peso = json_decode($respuestas->get($preguntaPeso->id)->respuesta);
 
             app('App\Http\Controllers\HomeController')->generarDieta($request->user(), $objetivo, $peso, $alimentosIgnorados, 1);
